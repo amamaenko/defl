ad764afa0c77effe71331a146167e456a4de55cb
Anton Mamaenko <amamaenko@gmail.com>
2017-03-28 22:07:00 +0300

    Update help strings for cli parameters.
    - move the help strings to separate constants in the cli module

--- defl/cli.py
+++ defl/cli.py
@@ -7,8 +7,15 @@ import argparse
 import defl.pathutil
 import defl.core
 
-HLP_DESC = """
+APP_HELP_STR = """
 Finds the duplicated image files 
+"""
+SOURCE_CMD_HELP_STR = """
+Specify the list of source directories that will be scanned for duplicates
+"""
+
+DRY_RUN_CMD_HELP_STR = """
+Prepare the command but not execute it. Print parsed arguments.
 """
 
 
@@ -16,21 +23,21 @@ def run():
     """Main entry point for parsing cli-arguments and launching the core
     function of the app.
     """
-    myparser = argparse.ArgumentParser(description=HLP_DESC)
+    myparser = argparse.ArgumentParser(description=APP_HELP_STR)
     myparser.add_argument(
-        "-f", "--folders", action="store",
+        "-s", "--source", action="store",
         default="",
         required=True,
-        help="Specify the list of folders whose content must be scanned",
+        help=SOURCE_CMD_HELP_STR,
         dest="src_folders")
     myparser.add_argument(
         "--dry-run", action='store_true',
         default=False,
-        help="Prepare the command but not execute it. Print parsed arguments.",
+        help=DRY_RUN_CMD_HELP_STR,
         dest="is_dry")
     flags = myparser.parse_args()
 
-    folder_names = defl.pathutil.str_to_foldernames(flags.src_folders)
+    folder_names = defl.pathutil.str_to_dirnames(flags.src_folders)
     if flags.is_dry:
         print(flags)
         print(folder_names)

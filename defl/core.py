0845dcaa31ab880a27734bad11e5214aff02bbda
Anton Mamaenko <amamaenko@gmail.com>
2017-04-08 21:23:49 +0300

    Fix progress indicator on md5 calculation

--- defl/core.py
+++ defl/core.py
@@ -70,7 +70,7 @@ def _filter_by_md5(file_info_pairs: FileInfoPairs)->FileInfoPairs:
             pbar.update()
             return _comp_md5(pair)
         filtered = filter(_progress_comp_md5, file_info_pairs)
-    result = list(filtered)
+        result = list(filtered)
     return result
 
 def _comp_md5(pair: FileInfoPair)->bool:
